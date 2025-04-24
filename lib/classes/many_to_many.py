ab1d4cb9a3c5b3f76b3d9cc601f07caff1858bab
Fiona <fionakanyi0@gmail.com>
2025-04-25 02:55:15 +0300

    lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,19 +1,15 @@
+
 class Article:
     all = []
 
     def __init__(self, author, magazine, title):
-        if not isinstance(title, str):
-            raise Exception("Title must be a string")
-        if not (5 <= len(title) <= 50):
-            raise Exception("Title must be between 5 and 50 characters")
+        if not (isinstance(title, str) and 5 <= len(title) <= 50):
+            raise ValueError("Title must be a string between 5 and 50 characters")
 
         self._title = title
         self.author = author
         self.magazine = magazine
         Article.all.append(self)
-
-        author._articles.append(self)
-        magazine._articles.append(self)
 
     @property
     def title(self):
@@ -22,26 +18,30 @@ class Article:
 class Author:
     def __init__(self, name):
         self._name = name
-        self._articles = []
 
     @property
     def name(self):
         return self._name
 
+    def articles(self):
+        return [article for article in Article.all if article.author == self]
+
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
+
     def add_article(self, magazine, title):
         return Article(self, magazine, title)
 
-    def articles(self):
-        return self._articles
+    def topic_areas(self):
+        if not self.articles():
+            return None
+        return list({article.magazine.category for article in self.articles()})
 
-    def magazines(self):
-        return list(set(article.magazine for article in self._articles))
 
 class Magazine:
     def __init__(self, name, category):
         self._name = name
         self._category = category
-        self._articles = []
 
     @property
     def name(self):
@@ -58,11 +58,21 @@ class Magazine:
 
     @category.setter
     def category(self, value):
-        if isinstance(value, str) and len(value) > 0:
+        if isinstance(value, str) and value:
             self._category = value
 
     def articles(self):
-        return self._articles
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        return list(set(article.author for article in self._articles))
+        return list({article.author for article in self.articles()})
+
+    def article_titles(self):
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
+
+    def contributing_authors(self):
+        from collections import Counter
+        author_counts = Counter(article.author for article in self.articles())
+        result = [author for author, count in author_counts.items() if count > 2]
+        return result if result else None
